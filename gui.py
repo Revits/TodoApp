ea31a99e0bd8b2a199aa20664331270e34fa29e9
Alif <alif.lukman99@gmail.com>
2025-09-08 21:50:45 +0700

    Create basic GUI with Add Interaction, 8/9/25 21:50

--- gui.py
+++ gui.py
@@ -2,10 +2,25 @@ import functions
 import FreeSimpleGUI as fsg
 
 label = fsg.Text("Type in a to-do")
-input_box = fsg.InputText(tooltip="Enter todo")
+input_box = fsg.InputText(tooltip="Enter todo", key="todo")
 add_button = fsg.Button("Add")
 
-window = fsg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = fsg.Window('My To-Do App',
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 15))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case fsg.WINDOW_CLOSED:
+            break
+
 window.close()
 
